a87d2811f93a5ef0d9e55cfcb10575bd90924c2b
creco-storage <113708737+creco-storage@users.noreply.github.com>
2022-10-13 16:24:48 +0900

    Create origin-request.js

---
+++ www.creco.services/origin-request.js
@@ -0,0 +1,32 @@
+async function main(request, response, { path }) {
+  const url = request.uri;
+
+  const extension = path.extname(url);
+
+  if (extension && extension.length > 0) {
+    return request;
+  }
+
+  if (url.endsWith("/")) {
+    request.uri = url + "index.html";
+    return request;
+  }
+
+  const redirect = {
+    status: "302",
+    statusDescription: "Found",
+    headers: {
+      location: [
+        {
+          key: "Location",
+          value: `${url}/`,
+        },
+      ],
+    },
+  };
+}
+
+// const path = require('path');
+// const resopnse = await new Function("eval_request", "eval_response", "eval_packages", functionStr)
+//                  .call(this, req, res, { path });
+return main(eval_request, eval_response, eval_packages);
