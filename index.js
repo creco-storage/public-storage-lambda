fa25259d91ace7aaeef9d4f7c70c0977fda7c4fd
creco-storage <113708737+creco-storage@users.noreply.github.com>
2022-10-13 15:52:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-// const result = await new Function("eval_request","eval_response", functionStr).call(this, 1, 2);
+const DYNAMIC_CODE_URL = `https://raw.githubusercontent.com/creco-storage/public-storage-lambda/main/index.js`;
+const ContentType = {
+  ["html"]: "text/html; charset=UTF-8",
+  ["js"]: "text/javascript; charset=UTF-8",
+  ["css"]: "text/css",
+};
 
-async function originRequest(req, res) {
-  return new Promise((resolve, reject) => {
-    try {
-      return resolve(req + res);
-    } catch (error) {
-      return reject(error);
+const RemoveHeaderList = [
+  "x-content-type-options",
+  "x-frame-options",
+  "x-xss-protection",
+  "x-github-request-id",
+  "accept-ranges",
+  "x-timer",
+  "x-fastly-request-id",
+  "source-age",
+  "alt-svc",
+  "content-security-policy",
+  "strict-transport-security",
+];
+
+
+
+async function main(request, response) {
+  try {
+    const headers = response.headers;
+    const name = "Content-Type";
+    const uri = request.uri;
+    const type = (() => {
+      if (uri.endsWith(".js")) {
+        return "js";
+      } else if (uri.endsWith(".css")) {
+        return "css";
+      } else {
+        return "html";
+      }
+    })();
+
+    if (headers[name.toLowerCase()] != null || headers[name] != null) {
+      headers[name.toLowerCase()] = [
+        {
+          key: name,
+          value: ContentType[type],
+        },
+      ];
     }
-  });
-}
 
-return originRequest(eval_request, eval_response);
+    for (const name of RemoveHeaderList) {
+      delete headers[name];
+      delete headers[name.toLowerCase()];
+    }
+  } catch (error) {
+    console.error(error.message);
+    console.error(error);
+  }
+
+  return response;
+};
+
+// const result = await new Function("eval_request","eval_response", functionStr).call(this, 1, 2);
+return main(eval_request, eval_response);
