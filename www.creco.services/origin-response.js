bf3bdb9ee19bf29b24be76fade12267d31e72d90
creco-storage <113708737+creco-storage@users.noreply.github.com>
2022-10-13 16:19:37 +0900

    Create origin-response.js

---
+++ www.creco.services/origin-response.js
@@ -0,0 +1,58 @@
+const ContentType = {
+  ["html"]: "text/html; charset=UTF-8",
+  ["js"]: "text/javascript; charset=UTF-8",
+  ["css"]: "text/css",
+};
+
+const RemoveHeaderList = [
+  "x-content-type-options",
+  "x-frame-options",
+  "x-xss-protection",
+  "x-github-request-id",
+  "accept-ranges",
+  "x-timer",
+  "x-fastly-request-id",
+  "source-age",
+  "alt-svc",
+  "content-security-policy",
+  "strict-transport-security",
+];
+
+async function main(request, response) {
+  try {
+    const headers = response.headers;
+    const name = "Content-Type";
+    const uri = request.uri;
+    const type = (() => {
+      if (uri.endsWith(".js")) {
+        return "js";
+      } else if (uri.endsWith(".css")) {
+        return "css";
+      } else {
+        return "html";
+      }
+    })();
+
+    if (headers[name.toLowerCase()] != null || headers[name] != null) {
+      headers[name.toLowerCase()] = [
+        {
+          key: name,
+          value: ContentType[type],
+        },
+      ];
+    }
+
+    for (const name of RemoveHeaderList) {
+      delete headers[name];
+      delete headers[name.toLowerCase()];
+    }
+  } catch (error) {
+    console.error(error.message);
+    console.error(error);
+  }
+
+  return response;
+};
+
+// const result = await new Function("eval_request","eval_response", functionStr).call(this, 1, 2);
+return main(eval_request, eval_response);
