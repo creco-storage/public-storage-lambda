ce0625483751a31ca09e984562f524d352b6d229
creco-storage <113708737+creco-storage@users.noreply.github.com>
2023-12-30 18:09:06 +0900

    Update origin-response.js

--- www.creco.services/origin-response.js
+++ www.creco.services/origin-response.js
@@ -22,9 +22,9 @@ const RemoveHeaderList = [
 async function main(request, response) {
   try {
     console.log(`status: ${response.status}`);
-    console.log(`host: ${request.headers['host'][0].value}`);
+    console.log(`host-2: ${request.headers['host'][0].value}`);
     if (response.status === '404') {
-      console.log(JSON.stringify(response, null, 2));
+      console.log(JSON.stringify(request, null, 2));
       response.status = '302';
       response.statusDescription = 'NotFound';
       response.headers['location'] = [
