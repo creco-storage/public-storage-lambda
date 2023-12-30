4662b519679f06e44c31b8b3186dd0f11b058d2b
creco-storage <113708737+creco-storage@users.noreply.github.com>
2023-12-30 18:49:19 +0900

    Update origin-response.js

--- www.creco.services/origin-response.js
+++ www.creco.services/origin-response.js
@@ -27,9 +27,11 @@ async function main(request, response) {
       const segments = request.uri.split('/');
 
       if (originDomain != null && originPath != null && segments.length > 1) {
+        console.log({ originDomain, originPath, segements_1: segements[1] });
         const url = `https://${originDomain}${originPath}/${segments[1]}/404/index.html`;
         const raw404 = await fetch(`https://raw.githubusercontent.com/creco-storage/public-storage/main/blog.creco.dev/`).then(x => x.text());
 
+        console.log({ raw404 });
         response.body = raw404;
       }
     }
